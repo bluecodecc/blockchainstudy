c613dcf1558811a592838504ba942f2b3f027cfc
zhouhuiqin <zhouhuiqin@bytedance.com>
2022-03-06 16:08:15 +0800

    use modifier to require

--- w2/contracts/Bank.sol
+++ w2/contracts/Bank.sol
@@ -1,18 +1,25 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
 contract Bank {
     address payable public owner;
-    mapping(address=>uint) public balances;
+    mapping(address => uint256) public balances;
+
     constructor() {
         owner = payable(msg.sender);
     }
+
+    //接受用户直接转账
     receive() external payable {
         balances[msg.sender] += msg.value;
     }
-    function withdraw(uint amount) external {
-        require(msg.sender == owner, "only owner can draw");
+    //只有所有者可以提现
+    function withdraw(uint256 amount) external onlyOwner {
         payable(msg.sender).transfer(amount);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can draw");
+        _;
     }
 }
